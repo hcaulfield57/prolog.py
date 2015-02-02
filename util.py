ea33ffbc0778f32014016c93e6597e632c1d99ae
Grant Mather <hcaulfield57@gmail.com>
2015-02-01 20:53:04 -0700

    removed some unnecessary functions from util.py

--- util.py
+++ util.py
@@ -1,17 +1,6 @@
 import datetime
 import sys
 
-def getdate():
-    today = datetime.date.today()
-    jdate = today - datetime.timedelta(13)
-    return jdate.month, jdate.day
-
-def isdefined(var):
-    if var is not None:
-        return True
-    else:
-        return False
-
 def parsedate(date):
     if len(date) != 5:
         usage()
